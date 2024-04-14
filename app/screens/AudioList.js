498f58d65ce88bcaff9a1b1ee074a89ef26be00e
eaguilar112 <erwin.aguilar123@outlook.com>
2024-04-14 16:17:11 -0600

    recycler

--- app/screens/AudioList.js
+++ app/screens/AudioList.js
@@ -1,13 +1,26 @@
 import React, { Component } from "react";
-import { View, StyleSheet, Text } from 'react-native';
+import { View, StyleSheet, Text, ScrollView, Dimensions } from 'react-native';
+import { AudioContext } from "../forcontext/AudioProvider";
+import { LayoutProvider, RecyclerListView } from 'recyclerlistview';
 
 export class AudioList extends Component {
+    static contextType = AudioContext;
+
+    layoutProvider = new LayoutProvider((i) => 'audio', (type, dim) => {
+        dim.width = Dimensions.get('window').width;
+        dim.height = 5;
+    })
+
+    rowRenderer = (type, item) => {
+        return <Text>{item.filename}</Text>
+    }
+
     render() {
-        return (
-            <View style={styles.container}>
-                <Text>Audio List</Text>
-            </View>
-        );
+        return <AudioContext.Consumer>
+            {({dataProvider}) => {
+                return <RecyclerListView dataProvider={dataProvider} layoutProvider={this.layoutProvider} rowRenderer={this.rowRenderer} />
+            }}
+        </AudioContext.Consumer>
     }
 }
 
@@ -16,7 +29,7 @@ const styles = StyleSheet.create({
         flex: 1,
         justifyContent: 'center',
         alignContent: 'center'
-    }
+    },
 })
 
 export default AudioList;
