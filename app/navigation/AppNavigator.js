14335ed217e3007dca2b2868d627cec2d14055b2
eaguilar112 <erwin.aguilar123@outlook.com>
2024-04-14 09:50:09 -0600

    p

--- app/navigation/AppNavigator.js
+++ app/navigation/AppNavigator.js
@@ -1,10 +1,17 @@
 import React from "react";
-import {createBottomTabNavigator} from '@react-navigation/native';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import AudioList from "../screens/AudioList";
+import Player from "../screens/Player";
+import Playlist from "../screens/Playlist";
+
+const Tab = createBottomTabNavigator()
 
 const AppNavigator = () => {
-    return (
-        
-    )
+    return <Tab.Navigator>
+        <Tab.Screen name='AudioList' component={AudioList} />
+        <Tab.Screen name='Player' component={Player} />
+        <Tab.Screen name='Playlist' component={Playlist} />
+    </Tab.Navigator>
 }
 
 export default AppNavigator;
