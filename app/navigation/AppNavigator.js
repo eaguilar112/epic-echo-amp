8802326e6487b55789407585d77eb1f458c2c508
eaguilar112 <erwin.aguilar123@outlook.com>
2024-04-14 10:03:00 -0600

    icons

--- app/navigation/AppNavigator.js
+++ app/navigation/AppNavigator.js
@@ -4,6 +4,8 @@ import AudioList from "../screens/AudioList";
 import Player from "../screens/Player";
 import Playlist from "../screens/Playlist";
 import { Ionicons } from '@expo/vector-icons';
+import { FontAwesome6 } from '@expo/vector-icons';
+import { MaterialIcons } from '@expo/vector-icons';
 
 const Tab = createBottomTabNavigator()
 
@@ -15,8 +17,17 @@ const AppNavigator = () => {
             options={{
             tabBarIcon: ({color, size}) => (<Ionicons name="headset" size={size} color={color} />)
         }} />
-        <Tab.Screen name='Player' component={Player} />
-        <Tab.Screen name='Playlist' component={Playlist} />
+        <Tab.Screen 
+            name='Player' 
+            component={Player} 
+            options={{
+                tabBarIcon: ({color, size}) => (<FontAwesome6 name="circle-play" size={size} color={color} />)
+        }} />
+        <Tab.Screen 
+            name='Playlist' 
+            component={Playlist} options={{
+                tabBarIcon: ({color, size}) => (<MaterialIcons name="playlist-play" size={size} color={color} />)
+        }} />
     </Tab.Navigator>
 }
 
