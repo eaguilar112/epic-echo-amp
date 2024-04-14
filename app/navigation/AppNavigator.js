9332b4825f57f0534f7d5aa16113861d695cda56
eaguilar112 <erwin.aguilar123@outlook.com>
2024-04-14 09:56:38 -0600

    icon1

--- app/navigation/AppNavigator.js
+++ app/navigation/AppNavigator.js
@@ -3,12 +3,18 @@ import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import AudioList from "../screens/AudioList";
 import Player from "../screens/Player";
 import Playlist from "../screens/Playlist";
+import { Ionicons } from '@expo/vector-icons';
 
 const Tab = createBottomTabNavigator()
 
 const AppNavigator = () => {
     return <Tab.Navigator>
-        <Tab.Screen name='AudioList' component={AudioList} />
+        <Tab.Screen 
+            name='AudioList' 
+            component={AudioList} 
+            options={{
+            tabBarIcon: ({color, size}) => (<Ionicons name="headset" size={size} color={color} />)
+        }} />
         <Tab.Screen name='Player' component={Player} />
         <Tab.Screen name='Playlist' component={Playlist} />
     </Tab.Navigator>
