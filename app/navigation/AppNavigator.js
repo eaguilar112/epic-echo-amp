ce1bf97e27fb0c85781b452f07b753ee335b24db
eaguilar112 <erwin.aguilar123@outlook.com>
2024-04-14 09:41:33 -0600

    pages

---
+++ app/navigation/AppNavigator.js
@@ -0,0 +1,10 @@
+import React from "react";
+import {createBottomTabNavigator} from '@react-navigation/native';
+
+const AppNavigator = () => {
+    return (
+        
+    )
+}
+
+export default AppNavigator;
