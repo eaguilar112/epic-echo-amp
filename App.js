4e7b7e71b3378f084a509e651485e12c15426d42
eaguilar112 <erwin.aguilar123@outlook.com>
2024-04-14 15:17:15 -0600

    adjusted

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import AppNavigator from './app/navigation/AppNavigator';
-import AudioProvider from './app/context/AudioProvider';
+import AudioProvider from './app/forcontext/AudioProvider';
 
 export default function App() {
   return (
