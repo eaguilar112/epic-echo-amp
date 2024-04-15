7fe3a4fe4cb4823f14e2f5e94414899cd95697c7
eaguilar112 <erwin.aguilar123@outlook.com>
2024-04-14 20:18:25 -0600

    com

--- App.js
+++ App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { View } from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
 import AppNavigator from './app/navigation/AppNavigator';
-import AudioProvider from './app/forcontext/AudioProvider';
+import AudioProvider from './app/context/AudioProvider';
 import AudioListItem from './app/components/AudioListItem';
 
 export default function App() {
--- app/screens/AudioList.js
+++ app/screens/AudioList.js
@@ -1,41 +1,12 @@
-import React, { Component } from "react";
-import { View, StyleSheet, Text, ScrollView, Dimensions } from 'react-native';
-import { AudioContext } from "../forcontext/AudioProvider";
-import { LayoutProvider, RecyclerListView } from 'recyclerlistview';
+import React from "react";
+import { View, StyleSheet, Text } from 'react-native';
 
-export class AudioList extends Component {
-    static contextType = AudioContext;
-
-    layoutProvider = new LayoutProvider(i => 'audio', (type, dim) => {
-        switch (type) {
-            case 'audio':
-                dim.width = Dimensions.get('window').width;
-                dim.height = 70;
-                break;
-            default: 
-                dim.width = 0;
-                dim.height = 0;
-            }
-        }   
-    );
-
-    rowRenderer = (type, item) => {
-        return <AudioList title={item.title} duration={item.duration} />
-    }
-
-    render() {
-        return (
-            <AudioContext.Consumer>
-            {({ dataProvider }) => {
-                return (
-                    <View style={{flex: 1}}>
-                        <RecyclerListView dataProvider={dataProvider} layoutProvider={this.layoutProvider} rowRenderer={this.rowRenderer} />
-                    </View>
-                );
-            }}
-            </AudioContext.Consumer>
-        );
-    }
+const AudioList = () => {
+    return (
+        <View style={styles.container}>
+            <Text>Audio List</Text>
+        </View>
+    )
 }
 
 const styles = StyleSheet.create({
@@ -43,7 +14,7 @@ const styles = StyleSheet.create({
         flex: 1,
         justifyContent: 'center',
         alignContent: 'center'
-    },
+    }
 })
 
 export default AudioList;
